605c176af48bf28a90ffbea2a03f1952036df47b
niteshsharmamothersoncom <nitesh.sharma@motherson.com>
2023-08-25 12:21:29 +0530

    *

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using SerilogDemo.Controllers;
 
 namespace NUnitTestProject
@@ -26,6 +28,42 @@ namespace NUnitTestProject
             HomeController home = new HomeController();
             string result = home.GetEmployeeName(empId);
             Assert.That(result, Is.EqualTo(name));
+        }
+
+
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+
+        IWebDriver m_driver;
+
+        [Test]
+        public void cssDemo()
+        {
+            m_driver = new ChromeDriver("D:\\");
+            m_driver.Url = "https://demo.guru99.com/test/guru99home/";
+            m_driver.Manage().Window.Maximize();
+            IWebElement link = m_driver.FindElement(By.XPath(".//*[@id='rt-header']//div[2]/div/ul/li[2]/a"));
+            link.Click();
+            m_driver.Close();
         }
     }
 }
--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -12,5 +12,6 @@ namespace XUnitTestProject
             Assert.Equal(str, str);
             Assert.True(true);
         }
+
     }
 }
